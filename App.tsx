c0a7f04c91b171f50a8fff5a2f53c37e521e71d8
purbagupta <purba@braincavesoft.com>
2024-03-06 10:17:53 +0530

    Build Problem solve

--- App.tsx
+++ App.tsx
@@ -19,7 +19,7 @@ export default function App() {
     return (
         <PaperProvider theme={CombinedDefaultTheme}>
         <NavigationContainer theme={CombinedDefaultTheme}>
-              <AppNavigation /> 
+              <AppNavigation />
             </NavigationContainer>
         </PaperProvider>
     );
